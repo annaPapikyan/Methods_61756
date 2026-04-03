ba972781b70236ad9e1f30dcf60bfe9a66b735f2
liorg <liorg@DESKTOP-PCO15MN>
2026-04-03 10:39:56 +0300

    shared data and threadcheckarray changes

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,43 +1,77 @@
+import java.util.ArrayList;
+
+/**
+ * Holds the shared information for our threads.
+ * Contains the list of numbers, the target number we are looking for (b),
+ * and variables to store the answer if we find it.
+ */
 public class SharedData 
 {
-	private int [] array;
+	private ArrayList<Integer> array;
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
 	
-	public SharedData(int[] array, int b) {
-		
+	/**
+	 * Creates the shared data object.
+	 * * @param array the list of numbers to check
+	 * @param b the target sum we want to reach
+	 */
+	public SharedData(ArrayList<Integer> array, int b) {
 		this.array = array;
 		this.b = b;
 	}
 
+	/**
+	 * Gets the array that shows which numbers are part of the answer.
+	 * * @return a boolean array with true for the winning numbers
+	 */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
 
+	/**
+	 * Updates the array with the right numbers for the solution.
+	 * * @param winArray the new boolean array to set
+	 */
 	public void setWinArray(boolean [] winArray) 
 	{
 		this.winArray = winArray;
 	}
 
-	public int[] getArray() 
+	/**
+	 * Gets our main list of numbers.
+	 * * @return the ArrayList of integers
+	 */
+	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
 
+	/**
+	 * Gets the target number.
+	 * * @return the number b
+	 */
 	public int getB() 
 	{
 		return b;
 	}
 
+	/**
+	 * Checks if a solution was already found by one of the threads.
+	 * * @return true if we found the answer, false if not
+	 */
 	public boolean getFlag() 
 	{
 		return flag;
 	}
 
+	/**
+	 * Changes the flag status. Usually called when a thread finds the right sum.
+	 * * @param flag the new status to set
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
-
 }
